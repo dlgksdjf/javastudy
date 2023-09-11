f5c55e10a2c90a70e6b609f46690855694edd310
SIST <SIST@DESKTOP-PJ43VCE>
2023-09-11 15:36:53 +0900

    12321

--- chapter01_project1/src/chapter01_project1/Mainclass.java
+++ chapter01_project1/src/chapter01_project1/Mainclass.java
@@ -4,7 +4,8 @@ public class Mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       System.out.println("GitHub");
+       System.out.println("kkkk");
 	}
 
 }
