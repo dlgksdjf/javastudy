50f2f9c7b0cc0d84937ddbcd87b9a782092f3174
SIST <SIST@DESKTOP-PJ43VCE>
2023-09-11 17:49:59 +0900

    1111111

---
+++ 2222/src/sss22.java
@@ -0,0 +1,14 @@
+
+public class sss22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Java!!");
+		System.out.println("hello Java!!");
+		System.out.println("hello Java!!");
+		System.out.println("hello Java!!");
+	        
+
+	}
+
+}
--- Chapter01_Project02/src/Mainclass.java
+++ Chapter01_Project02/src/Mainclass.java
@@ -1,6 +1,9 @@
 
-public class main {
+public class Mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(hello!!);
+
+	}
+
+}
